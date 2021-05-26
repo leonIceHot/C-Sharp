e195156d144e515f640b4f127dc729a340e8574d
LeonIceHot <hj098128@gmail.com>
2021-05-26 18:47:43 +0800

    Software Engineering Course Material

---
+++ 2021-05-26/Application.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shape a = new Shape { height = 100, width = 20, x = 3, y = 5 };
+            // Console.WriteLine(a.x);
+            // Shape a = new Shape();
+
+            Shape circle = new Circle();
+            circle.Draw();
+
+            Shape rectangle = new Rectangle();
+            rectangle.Draw();
+
+            Shape triangle = new Triangle();
+            triangle.Draw();
+
+            var shapes = new List<Shape>
+            {
+                new Circle(),
+                new Rectangle(),
+                new Triangle()
+            };
+
+            foreach(var shape in shapes) 
+            {
+                shape.Draw();
+            }
+        }
+
+        public class Shape
+        {
+            public int x { get; private set; }
+
+            public int y { get; private set; }
+
+            public int height { get; set; }
+
+            public int width { get; set; }
+
+            public int GetX()
+            {
+                x = 100;
+                return x;
+            }
+
+            public virtual void Draw()
+            {
+                WriteLine("Base class drawing ... \n");
+            }
+
+
+        }
+
+        public class Circle : Shape 
+        {
+            public override void Draw()
+            {
+                WriteLine("Circle class drawing...");
+                base.Draw();
+            }
+        }
+
+        public class Rectangle : Shape
+        {
+            public override void Draw()
+            {
+                WriteLine("Rectangle class drawing...");
+                base.Draw();
+            }
+        }
+
+        public class Triangle : Shape
+        {
+            public override void Draw()
+            {
+                WriteLine("Triangle class drawing...");
+                base.Draw();
+            }
+        }
+    }
+}
---
+++ 2021-05-26/Inheritance.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 3.0, h = 5.0;
+          
+            Shape c = new Circle(r);
+            Shape s = new Sphere(r);
+            Shape l = new Cylinder(r,h);
+            Shape squ = new Square(r, h);
+
+            Console.WriteLine("Area of Circle = {0:F2}", c.Area());
+            Console.WriteLine("Area of Circle = {0:F2}", s.Area());
+            Console.WriteLine("Area of Circle = {0:F2}", l.Area());
+            Console.WriteLine("Area of Circle = {0:F2}", squ.Area());
+        }
+        public class Shape
+        {
+            public const double PI = Math.PI;
+            protected double x, y;
+
+            public Shape()
+            {
+
+            }
+
+            public Shape(double x,double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public virtual double Area()
+            {
+                return x * y;
+            }
+
+        }
+        
+        public class Circle : Shape 
+        {
+            public Circle(double r):base(r,0)
+            {
+
+            }
+
+            public override double Area()
+            {
+                return PI * x * x;                
+            }
+        }
+
+        class Sphere : Shape
+        {
+            public Sphere(double r) : base(r, 0) 
+            {
+            }
+
+            public override double Area()
+            {
+                return 4 * PI * x * x;
+            }
+
+        }
+
+        class Cylinder : Shape 
+        {
+            public Cylinder(double r ,double h):base(r,h)
+            {
+
+            }
+
+            public override double Area()
+            {
+                return 2 * PI * x * x + 2 * PI * x * y;
+            }
+        }
+
+        class Square : Shape
+        {
+            public Square(double r, double h) : base(r, h)
+            {
+
+            }
+
+            public override double Area()
+            {
+                return x * y;
+            }
+        }
+    }
+}
